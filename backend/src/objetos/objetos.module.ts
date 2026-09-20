6fdbd87791dcf1b99d350df5dea98482685b065e
JAdilsonSA <jadilson_aroeira@outlook.com>
2026-09-20 00:06:13 -0300

    feat: adiciona gerenciamento e devolucao de objetos

--- backend/src/objetos/objetos.module.ts
+++ backend/src/objetos/objetos.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+
 import { ObjetosRepositoryService } from './objetos-repository/objetos-repository.service.js';
 import { CadastroController } from './cadastro/cadastro.controller.js';
 import { CadastroService } from './cadastro/cadastro.service.js';
 
+import { GerenciamentoController } from './gerenciamento/gerenciamento.controller.js';
+import { GerenciamentoService } from './gerenciamento/gerenciamento.service.js';
+
 @Module({
-  providers: [ObjetosRepositoryService, CadastroService],
+  providers: [
+    ObjetosRepositoryService,
+    CadastroService,
+    GerenciamentoService,
+  ],
+
   exports: [ObjetosRepositoryService],
-  controllers: [CadastroController],
+
+  controllers: [
+    CadastroController,
+    GerenciamentoController,
+  ],
 })
 export class ObjetosModule {}
