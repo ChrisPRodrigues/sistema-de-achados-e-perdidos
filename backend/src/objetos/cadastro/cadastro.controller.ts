8ad32ae5e20c0c403037adc441e1a23d19e9cbcf
ChrisPRodrigues <iaracristina.pr@outlook.com>
2026-09-19 22:45:55 -0300

    feat: implementa cadastro de objetos

---
+++ backend/src/objetos/cadastro/cadastro.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CadastroService } from './cadastro.service.js';
+import type { Objeto } from '../models/objeto/objeto.interface.js';
+
+@Controller('objetos')
+export class CadastroController {
+  constructor(private readonly cadastroService: CadastroService) {}
+
+  @Post()
+  cadastrar(@Body() dados: Omit<Objeto, 'id' | 'status'>): Objeto {
+    return this.cadastroService.cadastrar(dados);
+  }
+}
